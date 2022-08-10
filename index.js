7e3bfade54306390b298f3fef367fd0240ea6363
jvb <vvimoink@gmail.com>
2022-08-10 15:23:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-vue",
+    "stylelint-config-prettier",
+    "stylelint-config-recess-order",
+  ],
+};
